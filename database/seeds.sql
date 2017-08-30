320e2e9d3b16136f531ba7c1cb40525f92fd459e
hnguy0221 <hnguy0221@gmail.com>
2017-08-30 13:21:12 -0600

    removed select sq from seeds.sql

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,7 +1,7 @@
 use sizzleRegistrationDb;
 INSERT INTO courses (crs_nm, crs_ds, crs_dys, crs_str_dt, crs_stp_dt, crs_str_tm, crs_end_tm, LocationId, createdAt, updatedAt)
-VALUES ('Cullinary 101: Intro to Sautéing Poultry', 'This course builds the foundation of cooking poultry using the sauté method. Here you will discover the origins of sauté and why it is used to prepare various meats for consumption.', 'M W', '2017-10-02 17:30', '2017-10-16 21:30', '17:30', '21:30', current_date(), current_date()),
-('Restaurant Management 203: Front of House', 'This course is focused on providing each student with an understanding of the importance of an organized Front-of-House and even puts the student in the position caring for their own example.', 'M W', '20171002 17:30', '20171016 21:30',  '17:30', '21:30', current_date(), current_date()),
+VALUES ('Cullinary 101: Intro to Sautéing Poultry', 'This course builds the foundation of cooking poultry using the sauté method. Here you will discover the origins of sauté and why it is used to prepare various meats for consumption.', 'M W', '2017-10-02 17:30', '2017-10-16 21:30', '17:30', '21:30', 1, current_date(), current_date()),
+('Restaurant Management 203: Front of House', 'This course is focused on providing each student with an understanding of the importance of an organized Front-of-House and even puts the student in the position caring for their own example.', 'M W', '20171002 17:30', '20171016 21:30',  '17:30', '21:30', 1, current_date(), current_date()),
 ('Culinary 205: Health Code Regulations', 'This course helps the student to familiarize themselves with government regulations regarding public health and safety in the restaurant environment.', 'M W', '20171002 17:30', '20171016 21:30', '17:30', '21:30', 1, current_date(), current_date());
 
 INSERT INTO locations (loc_nm, loc_ds, room_no, createdAt, updatedAt) 
@@ -13,5 +13,4 @@ INSERT INTO administrators (fst_nm, lst_nm, password, createdAt, updatedAt) VALU
 
 /*UPDATE administrators set password = '123' where id = 1;*/
 
-select * from courses as Course;
 
